3dabb61cc70bdc03a006429e1094c3e3a4f4c33c
Riccardo Sieve <sievericcardo@gmail.com>
2021-07-29 11:56:47 +0200

    Create ManageArticles.jsx
    Added the page to add or modify articles

---
+++ src/components/dashboard/ManageArticles.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import AddArticle from '../articles/AddArticle';
+import ListArticles from '../articles/ListArticles';
+
+const ManageArticles = () => {
+    const auth = useSelector(state => state.auth);
+
+    const [ article, setArticle ] = useState({
+        name: "",
+        isComplete: false
+    });
+
+    if(!auth._id) {
+        return <Redirect to="/signin" />
+    }
+
+    return (
+        <>
+            <AddArticle article={article} setArticle={setArticle} />
+            <ListArticles setArticle={setArticle} />
+        </>
+    );
+};
+
+export default ManageArticles;
