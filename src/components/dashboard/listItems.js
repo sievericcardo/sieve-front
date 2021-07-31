5094b93d865f2cc614d1169a7d3d574220b55edf
Riccardo Sieve <sievericcardo@gmail.com>
2021-07-31 17:06:54 +0200

    Update listItems.js
    Set the proper routes

--- src/components/dashboard/listItems.js
+++ src/components/dashboard/listItems.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
 
 // Import element from the core
 import ListItem from '@material-ui/core/ListItem';
@@ -8,33 +11,43 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 
 // Import icons
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import LayersIcon from '@material-ui/icons/Layers';
+// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+// import PeopleIcon from '@material-ui/icons/People';
+// import BarChartIcon from '@material-ui/icons/BarChart';
+// import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+
+// Add icons for data in dashboard
+import Book from '@material-ui/icons/Book';
+import AccountTree from '@material-ui/icons/AccountTree';
 
 export const mainListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Orders" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItem>
-    <ListItem button>
+    <Link className="linkStyle" to='/cms-dashboard'>
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </Link>
+    <Link className="linkStyle" to='/cms-dashboard/manage-articles'>
+      <ListItem button>
+        <ListItemIcon>
+         <Book />
+        </ListItemIcon>
+        <ListItemText primary="Articles" />
+      </ListItem>
+    </Link>
+    <Link className="linkStyle" to='/cms-dashboard/manage-projects'>
+      <ListItem button>
+        <ListItemIcon>
+          <AccountTree />
+        </ListItemIcon>
+        <ListItemText primary="Projects" />
+      </ListItem>
+    </Link>
+    {/* <ListItem button>
       <ListItemIcon>
         <BarChartIcon />
       </ListItemIcon>
@@ -45,7 +58,7 @@ export const mainListItems = (
         <LayersIcon />
       </ListItemIcon>
       <ListItemText primary="Integrations" />
-    </ListItem>
+    </ListItem> */}
   </div>
 );
 
