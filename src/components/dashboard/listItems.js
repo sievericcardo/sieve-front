f7aa5f00764c4a965667673b0c1baa1c60f89698
Riccardo Sieve <sievericcardo@gmail.com>
2021-07-31 19:08:16 +0200

    Update listItems.js
    Removed unused data

--- src/components/dashboard/listItems.js
+++ src/components/dashboard/listItems.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
 
 import { Link } from 'react-router-dom';
 
@@ -11,10 +10,6 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 
 // Import icons
 import DashboardIcon from '@material-ui/icons/Dashboard';
-// import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-// import PeopleIcon from '@material-ui/icons/People';
-// import BarChartIcon from '@material-ui/icons/BarChart';
-// import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 
 // Add icons for data in dashboard
@@ -47,18 +42,6 @@ export const mainListItems = (
         <ListItemText primary="Projects" />
       </ListItem>
     </Link>
-    {/* <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItem> */}
   </div>
 );
 
