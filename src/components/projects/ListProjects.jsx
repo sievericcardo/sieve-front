d0157332ccdd3af30afe05fcf7f1a4c05d973390
Riccardo Sieve <sievericcardo@gmail.com>
2021-07-31 21:46:20 +0200

    Create ListProjects.jsx
    Created file for front

---
+++ src/components/projects/ListProjects.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import Project from './Project';
+import { getProjects } from '../../store/actions/projectActions';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const ListProjects = ({ project, setProject }) => {
+  const classes = useStyles();
+
+  const dispatch = useDispatch();
+  const projects = useSelector((state) => state.projects);
+
+  var length = 0;
+
+  if (!projects) {
+    length = 0
+  } else {
+    length = projects.length
+  }
+
+  // Use Effect will be called when our components renders
+  useEffect(() => {
+    dispatch(getProjects());
+  }, [dispatch]); // this is to avoid it making continually rendering
+
+  return (
+    <div className={classes.root}>
+    {length > 0 ? (
+      <Grid container spacing={3}>
+        {projects &&
+          projects.map((project) => {
+            return (
+              <Grid item xs={3}>
+                <Project
+                  project={project}
+                  key={project._id}
+                  setProject={setProject}
+                />
+              </Grid>
+            );
+          })}
+      </Grid>
+    ) : ""}
+    </div>
+  );
+}
+
+export default ListProjects;
