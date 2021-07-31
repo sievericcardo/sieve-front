505606b3d7af9ab1fdaf9da25c0249e28a9d4309
Riccardo Sieve <sievericcardo@gmail.com>
2021-07-31 21:46:36 +0200

    Update Projects.jsx
    Modified file for project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import ListProjects from './ListProjects';
 
 const Projects = () => {
+  const [project, setProject] = useState({
+    name: "",
+    body: "",
+  });
+
   return (
     <div>
-      {/* < */}
+      <ListProjects
+        project={project} setProject={setProject} 
+      />
     </div>
   );
 };
