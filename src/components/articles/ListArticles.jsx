36adc80aac50cd82f8f7462a89c0c5efb3e6a218
Riccardo Sieve <sievericcardo@gmail.com>
2021-07-31 21:45:45 +0200

    Create ListArticles.jsx
    New file for front

---
+++ src/components/articles/ListArticles.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { makeStyles } from '@material-ui/core/styles';
+import ImageList from '@material-ui/core/ImageList';
+// import ImageListItem from '@material-ui/core/ImageListItem';
+// import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+// import IconButton from '@material-ui/core/IconButton';
+
+// import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+import Article from './Article';
+import { getArticles } from '../../store/actions/articleActions';
+
+// import itemData from './itemData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
+
+const ListArticles = ({ setArticle }) => {
+  const classes = useStyles();
+
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.articles);
+
+  var length = 0;
+
+  if (!articles) {
+    length = 0
+  } else {
+    length = articles.length
+  }
+
+  // Use Effect will be called when our components renders
+  useEffect(() => {
+    dispatch(getArticles());
+  }, [dispatch]); // this is to avoid it making continually rendering
+
+  return (
+    <div className={classes.root}>
+    {length > 0 ? (
+      <ImageList className={classes.imageList} cols={2.5}>
+        {articles &&
+          articles.map((article) => {
+            return (
+              <Article
+                article={article}
+                key={article._id}
+                setArticle={setArticle}
+              />
+            );
+          })}
+      </ImageList>
+      ) : ""}
+    </div>
+  );
+}
+
+export default ListArticles;
