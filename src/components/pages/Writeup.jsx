42360ae9e92fcf7284faca0a94b6499d06e2a0e6
Riccardo Sieve <sievericcardo@gmail.com>
2021-10-15 15:53:01 +0200

    Create Writeup.jsx
    Added code for single page writeup

---
+++ src/components/pages/Writeup.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+// import { NavLink } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown'
+
+// Single post component
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/styles";
+
+import { url } from '../../api/index';
+
+const useStyles = makeStyles({
+  mainContent: {
+    width: "75vw",
+    margin: "auto",
+    paddingTop: "3em",
+  },
+});
+
+const  Writeup = () => {
+  const classes = useStyles();
+
+  // Get ID from URL
+  const { id } = useParams();
+  const [writeup, setWriteup] = useState([]);
+
+  useEffect(()=> {
+      axios.get(`${url}/writeups/${id}`)
+      .then(res => {
+          console.log(res)
+          setWriteup(res.data)
+      })
+      .catch(err =>{
+          console.log(err)
+      })
+  }, [id]);
+
+  return (
+    <div className={classes.mainContent}>
+      {/* <ReactMarkdown>{ decodeURIComponent(escape(atob(writeup.body))) }</ReactMarkdown> */}
+      <ReactMarkdown>{ decodeURIComponent(escape(atob(writeup.body))) }</ReactMarkdown>
+    </div>
+  );
+};
+
+export default Writeup;
