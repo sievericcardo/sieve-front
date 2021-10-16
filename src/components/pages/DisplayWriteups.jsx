94418ab7b6e4fd6564fa38d891c976cbcd852985
Riccardo Sieve <sievericcardo@gmail.com>
2021-10-16 19:41:58 +0200

    Update DisplayWriteups.jsx
    Set banner for challenge

--- src/components/pages/DisplayWriteups.jsx
+++ src/components/pages/DisplayWriteups.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Steg from '../../assets/img/base-img/steghide-is-fun.png';
 
 import { makeStyles } from '@material-ui/core';
 
@@ -12,6 +13,10 @@ const useStyles = makeStyles({
     width: '80vw',
     margin: 'auto',
   },
+  imageBanner: {
+    marginTop: '2.4em',
+    textAlign: 'center',
+  },
 });
 
 const DisplayWriteups = () => {
@@ -23,10 +28,12 @@ const DisplayWriteups = () => {
         <h1>Writeups</h1>
         <p>&nbsp;</p>
         <p>During my experience in HackTheBox and TryHackMe I've been able to get lot of experience and knowledge regarding the cybersecurity domain; I hope that my writeups can be of help to learn something as well.</p>
-        <p>&nbsp;</p>
         <p>I will try to add more writeups as the time passes to provide more and more content to see; for any information, suggestion contact me and I'll be happy to discuss about cyber topics.</p>
         <p>&nbsp;</p>
         <Writeups />
+        <div className={ classes.imageBanner }>
+          <img src={ Steg } alt="Steganography is fun" />
+        </div>
       </div>
     </div>
   )
