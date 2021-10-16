0f0bd14c5e0b6ef996cdd6c8fb5667709defe38a
Riccardo Sieve <sievericcardo@gmail.com>
2021-10-16 19:07:09 +0200

    Update Writeup.jsx
    added semicolon

--- src/components/pages/Writeup.jsx
+++ src/components/pages/Writeup.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 // import { NavLink } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
 
 // Single post component
 import axios from "axios";
